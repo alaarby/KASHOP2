01847b004dee36f3128d3e8abdc1ced71e2ce828
alaa rabaya <alaarabaya307@gmail.com>
2026-01-28 14:33:45 +0200

    stripe and orders

--- KASHOP2.BLL/obj/Debug/net9.0/KASHOP2.BLL.AssemblyInfo.cs
+++ KASHOP2.BLL/obj/Debug/net9.0/KASHOP2.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KASHOP2.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d17bcf2c5336f9cf6cb0baac8bcaaadafe81f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e73290c4eaf8c433b3c93e690259734f51118a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KASHOP2.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KASHOP2.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KASHOP2.DAL/obj/Debug/net9.0/KASHOP2.DAL.AssemblyInfo.cs
+++ KASHOP2.DAL/obj/Debug/net9.0/KASHOP2.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KASHOP2.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d17bcf2c5336f9cf6cb0baac8bcaaadafe81f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e73290c4eaf8c433b3c93e690259734f51118a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KASHOP2.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KASHOP2.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
